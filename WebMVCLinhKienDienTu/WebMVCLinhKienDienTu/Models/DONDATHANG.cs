3b154cc4b778fc9266e761c174ac2f92e3300c7b
Ngoc long <ngoclong923363@gmail.com>
2017-01-05 08:59:55 +0700

    Do an do nhom khac lam

---
+++ WebMVCLinhKienDienTu/WebMVCLinhKienDienTu/Models/DONDATHANG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCLinhKienDienTu.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class DONDATHANG
+    {
+        public int MaDonHang { get; set; }
+        public Nullable<bool> Dathanhtoan { get; set; }
+        public Nullable<bool> Tinhtranggiaohang { get; set; }
+        public Nullable<System.DateTime> Ngaydat { get; set; }
+        public Nullable<System.DateTime> Ngaygiao { get; set; }
+        public string Id { get; set; }
+
+        public virtual AspNetUser AspNetUser { get; set; }
+    }
+}
